b87f4e90722e072f58d4974b594b87bc9ae91bb4
Otavio Palma <otaviopalma@gmail.com>
2021-04-01 13:57:20 -0300

    feat: project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import Routes from './routes/routes';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+import Search from '../pages/Search';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact />
-      <Route path="/search" />
+      <Route path="/" exact component={Home} />
+      <Route path="/search" component={Search} />
     </Switch>
   </BrowserRouter>
 );
